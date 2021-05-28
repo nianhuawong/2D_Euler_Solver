dca240a02195f52db7cf7dc4dce42eee84988eb7
nianhuawong <nianhuawong@126.com>
2021-05-28 21:01:32 +0800

    2维Euler方程求解器，初始版本

---
+++ 2D_Euler_Solver.cpp
@@ -0,0 +1,45 @@
+﻿// 2D_Euler_Solver.cpp: 定义应用程序的入口点。
+//
+
+#include "2D_Euler_Solver.h"
+
+using namespace std;
+
+int main(int argc, char ** argv )
+{
+	Simulation * two_dim_Euler_Solver = new Simulation();
+
+	two_dim_Euler_Solver->Run();
+
+	delete two_dim_Euler_Solver;
+
+	return 0;
+}
+
+void Simulation::Run()
+{
+	//建立全局参数、变量、流场
+	Init_Global_Param();
+
+	Generate_Mesh();
+
+	Flow_Initialization();
+
+
+	for (iter = 0; iter < numberOfTimeSteps; ++iter)
+	{
+		//先计算x方向
+		//Load_Q();
+
+		//Half_Node_Q();
+
+		//Half_Node_Flux();
+
+		//Spatial_Derivative();
+
+		//Time_Integral();
+	}
+
+	//再计算y方向
+
+}
---
+++ 2D_Euler_Solver.h
@@ -0,0 +1,24 @@
+﻿// 2D_Euler_Solver.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+
+class Simulation
+{
+public:
+	Simulation() {}
+	~Simulation(){}
+
+public:
+	void Run();
+
+};
+int iter, numberOfTimeSteps;
+
+void Init_Global_Param() {};
+void Generate_Mesh() {};
+void Flow_Initialization() {};
+
+// TODO: 在此处引用程序需要的其他标头。
